2d3fdb2b5ae3e9778ae322c1489f3c7bfe0dc7eb
Lorena Vilaça <lcjbv2@cin.ufpe.br>
2023-11-22 14:47:26 -0300

    adding project

---
+++ Deu Quanto/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Deu Quanto
+//
+//  Created by LoreVilaca on 21/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .blue
+    }
+
+
+}
+
